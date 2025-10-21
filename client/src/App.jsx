d4ae201e05d82dff879219be641d1ecdf3612879
anishdass <anishdassatoffice@gmail.com>
2025-10-21 15:40:09 +0100

    Hero section completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import Preview from "./pages/Preview";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='app' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='builder/:resumeId' element={<ResumeBuilder />} />
+        </Route>
+        <Route path='view/:resumeId' element={<Preview />} />
+        <Route path='login' element={<Login />} />
       </Routes>
     </>
   );
