51d21e4c0562bbfe61846287639c73c1de168bb0
anishdass <anishdassatoffice@gmail.com>
2025-10-22 14:59:57 +0100

    Call to action section completed

--- client/src/components/home/CallToAction.jsx
+++ client/src/components/home/CallToAction.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const CallToAction = () => {
   return (
-    <div className='border-y border-dashed border-slate-200 w-full max-w-5xl mx-auto px-10 sm:px-16'>
+    <div
+      id='cta'
+      className=' mt-28 border-y border-dashed border-slate-200 w-full max-w-5xl mx-auto px-10 sm:px-16'>
       <div className='flex flex-col md:flex-row text-center md:text-left items-center justify-between gap-8 px-3 md:px-10 border-x border-dashed border-slate-200 py-16 sm:py-20 -mt-10 -mb-10 w-full'>
         <p className='text-xl font-medium max-w-md text-slate-800'>
-          Star the repo and join the growing PrebuiltUI community.
+          Build A Professional Resume That Helps You Stand Out And Get Hired
         </p>
         <a
           href='https://prebuiltui.com'
-          className='flex items-center gap-2 rounded py-3 px-8 bg-indigo-600 hover:bg-indigo-700 transition text-white'>
-          <span>Star On Github</span>
+          className='flex items-center gap-2 rounded py-3 px-8 bg-green-600 hover:bg-green-700 transition text-white'>
+          <span>Get Stared</span>
           <svg
             xmlns='http://www.w3.org/2000/svg'
             width='24'
