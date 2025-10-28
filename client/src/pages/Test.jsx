311e30d2a3561e81e99a1aa7cff435ad934ebebe
anishdass <anishdassatoffice@gmail.com>
2025-10-28 15:47:08 +0000

    Professional Summary Section completed

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,56 +1,7 @@
-import { Check, Palette } from "lucide-react";
-import React, { useState } from "react";
+import React from "react";
 
-const Test = ({ selectedColor, onChange }) => {
-  const colors = [
-    { name: "Blue", value: "#3b82f6" },
-    { name: "Indigo", value: "#6366f1" },
-    { name: "Purple", value: "#8b5cf6" },
-    { name: "Green", value: "#10b981" },
-    { name: "Red", value: "#ef4444" },
-    { name: "Orange", value: "#f97316" },
-    { name: "Teal", value: "#14b8a6" },
-    { name: "Pink", value: "#ec4899" },
-    { name: "Gray", value: "#6b7280" },
-    { name: "Black", value: "#1f2937" },
-  ];
-
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <div className='flex justify-center items-center relative h-full'>
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className='flex items-center gap-1 text-sm text-purple-600 bg-gradient-to-br from-purple-50 to-purple-100 ring-purple-300 hover:ring transition-all px-3 py-2 rounded-lg'>
-        <Palette size={16} />
-        <span className='max-sm:hidden'>Accent</span>
-      </button>
-
-      {isOpen && (
-        <div className='grid grid-cols-4 w-60 gap-2 absolute top-full left-1/2 -translate-x-1/2 p-3 mt-2 z-10 bg-white rounded-md border border-gray-200 shadow-sm'>
-          {colors.map((color) => (
-            <div
-              key={color.value}
-              className='relative cursor-pointer group flex flex-col'
-              onClick={() => onChange(color.value)}>
-              <div
-                className='w-12 h-12 rounded-full border-2 border-transparent group-hover:border-black/25 transition-colors'
-                style={{ backgroundColor: color.value }}
-              />
-              {selectedColor === color.value && (
-                <div className='absolute inset-0 flex items-center justify-center'>
-                  <Check className='size-5 text-white' />
-                </div>
-              )}
-              <p className='text-xs text-center mt-1 text-gray-600'>
-                {color.name}
-              </p>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+const Test = () => {
+  return <div></div>;
 };
 
 export default Test;
