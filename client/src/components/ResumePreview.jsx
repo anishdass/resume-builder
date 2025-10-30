e7aab6ac55a58bbfd2bf43ffd3ca86b741273aee
anishdass <anishdassatoffice@gmail.com>
2025-10-30 12:58:31 +0000

    Preview page and the frontend part completed

--- client/src/components/ResumePreview.jsx
+++ client/src/components/ResumePreview.jsx
@@ -4,65 +4,69 @@ import ModernTemplate from "./templates/ModernTemplate";
 import MinimalTemplate from "./templates/MinimalTemplate";
 import MinimalImageTemplate from "./templates/MinimalImageTemplate";
 
-const ResumePreview = ({ data, template, accentColor, classes = "" }) => {
+const ResumePreview = ({ data, classes = "" }) => {
   const renderTemplate = () => {
-    switch (template) {
+    switch (data.template) {
       case "modern":
-        return <ModernTemplate data={data} accentColor={accentColor} />;
+        return <ModernTemplate data={data} accentColor={data.accent_color} />;
 
       case "minimal":
-        return <MinimalTemplate data={data} accentColor={accentColor} />;
+        return <MinimalTemplate data={data} accentColor={data.accent_color} />;
 
       case "minimal-image":
-        return <MinimalImageTemplate data={data} accentColor={accentColor} />;
+        return (
+          <MinimalImageTemplate data={data} accentColor={data.accent_color} />
+        );
 
       default:
-        return <ClassicTemplate data={data} accentColor={accentColor} />;
+        return <ClassicTemplate data={data} accentColor={data.accent_color} />;
     }
   };
 
   return (
-    <div className=' w-full bg-gray-100'>
-      <div
-        id='resume-preview'
-        className={
-          " border border-gray-200 print:shadow-none print:border-none" +
-          classes
-        }>
-        {renderTemplate()}
-      </div>
-      <style jsx>{`
-        @page {
-          size: letter;
-          margin: 0;
-        }
-        @media print {
-          html,
-          body {
-            width: 8.5in;
-            height: 11in;
-            overflow: hidden;
-          }
-          body* {
-            visibility: hidden;
-          }
-          #resume-preview #resume-preview * {
-            visibility: visible;
-          }
-          #resume-preview {
-            position: absolute;
-            left: 0;
-            top: 0;
-            width: 100;
-            height: auto;
+    data && (
+      <div className=' w-full bg-gray-100'>
+        <div
+          id='resume-preview'
+          className={
+            " border border-gray-200 print:shadow-none print:border-none" +
+            classes
+          }>
+          {renderTemplate()}
+        </div>
+        <style jsx>{`
+          @page {
+            size: letter;
             margin: 0;
-            padding: 0;
-            box-shadow: none !important;
-            border: none !important;
           }
-        }
-      `}</style>
-    </div>
+          @media print {
+            html,
+            body {
+              width: 8.5in;
+              height: 11in;
+              overflow: hidden;
+            }
+            body* {
+              visibility: hidden;
+            }
+            #resume-preview #resume-preview * {
+              visibility: visible;
+            }
+            #resume-preview {
+              position: absolute;
+              left: 0;
+              top: 0;
+              width: 100;
+              height: auto;
+              margin: 0;
+              padding: 0;
+              box-shadow: none !important;
+              border: none !important;
+            }
+          }
+        `}</style>
+      </div>
+    )
   );
 };
 
--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,28 +1,7 @@
-import React, { useReducer } from "react";
-
-const initialState = { count: 0 };
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    default:
-      return state;
-  }
-}
+import React from "react";
 
 const Test = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  return (
-    <div style={{ textAlign: "center" }}>
-      <h2>Count: {state.count}</h2>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Test;
