a0731c9628d44181f4fcb82bf45b32e88445e16f
anishdass <anishdassatoffice@gmail.com>
2025-11-01 15:58:59 +0000

    User APIs completed.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -11,6 +11,7 @@ const generateToken = (userId) => {
   return token;
 };
 
+// Controller for user registration
 // Post: /api/users/registers
 export const registerUser = async (req, res) => {
   try {
@@ -44,5 +45,54 @@ export const registerUser = async (req, res) => {
       .json({ message: "User created successfully", token, user: newUser });
   } catch (error) {
     return res.status(400).json({ message: error.message });
+  }
+};
+
+// Controller for user login
+// POST: /api/users/login
+export const loginUser = async (req, res) => {
+  try {
+    // Get email and password from the body
+    const { email, password } = req.body;
+
+    // Check if user exists. If false send 400 and the message invalid user or password
+    const user = User.findOne(email);
+    if (!user) {
+      res.status(400).json({ message: "Invalid email or passsword" });
+    }
+    // Verify password. If correct send a success message
+    if (!user.password) {
+      res.status(400).json({ message: "Invalid Password" });
+    }
+
+    // Generate token using user id, set user password as undefined, return user created successful message
+    const token = generateToken(user._id);
+    user.password = undefined;
+
+    return res.status(200).json({ message: "Login successfull", token, user });
+  } catch (error) {
+    // Catch block with error message
+    return res.status(200).json({ message: error.message });
+  }
+};
+
+// Controller for user by id
+// POST: /api/users/data
+export const getUserById = async (req, res) => {
+  try {
+    // Get email and password from the body
+    const { userId } = req.userId;
+
+    // Check if user exists
+    const user = User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    
+    // Return user
+    return res.status(200).json({ user });
+  } catch (error) {
+    // Catch block with error message
+    return res.json(400).json({ message: error.message });
   }
 };
