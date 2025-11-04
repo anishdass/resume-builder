ccc4a96a40a3ccfc33ed3c82d7ded444605dd616
anishdass <anishdassatoffice@gmail.com>
2025-11-04 13:04:37 +0000

    Redux components for state management in place

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
           <Route path='builder/:resumeId' element={<ResumeBuilder />} />
         </Route>
         <Route path='view/:resumeId' element={<Preview />} />
-        <Route path='login' element={<Login />} />
+
         <Route path='test' element={<Test />} />
       </Routes>
     </>
