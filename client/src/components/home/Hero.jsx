7305ed90352522f4443a255e37ca47fc2112deb7
anishdass <anishdassatoffice@gmail.com>
2025-11-04 13:19:35 +0000

    Login button in Hero section customized as per the redux states

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -42,11 +42,11 @@ function Hero() {
               className='hidden md:block px-6 py-2 bg-green-700 hover:bg-green-700 active:scale-95 transition-all rounded-full text-white'>
               Get started
             </Link>
-            <a
+            <Link
               to='/app?state=login'
               className='hidden md:block px-6 py-2 border active:scale-95 hover:bg-slate-50 transition-all rounded-full text-slate-700 hover:text-slate-900'>
               Login
-            </a>
+            </Link>
           </div>
 
           <button
